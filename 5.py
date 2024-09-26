55e9eab81fb37d11c382059407c097176c90af3e
monkeyboss <D1345515@o365.fcu.edu.tw>
2024-09-26 11:07:25 +0800

    add 5.py file

--- 5.py
+++ 5.py
@@ -1,3 +1,5 @@
-a=int(input('輸⼊數字：'))
-a%2==0 and print('偶數') 
-a%2!=0 and print('奇數')
+string=int(input('輸⼊三位數：'))
+a=int(string//100)
+b=int(string//10)-a*10
+c=int(string%10)
+print(c,b,a,sep='')
